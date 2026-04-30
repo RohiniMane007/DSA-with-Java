aaa58bc835b945e5004172a5113011d06c61e5d5
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2026-04-30 17:20:58 +0530

    Function
    Refactored Function.java to include main method, factorial, isPrime, and listPrimeNo methods.

--- Function.java
+++ Function.java
@@ -1,17 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 
-int factorial(int n){
-  // int n = 5;
-  int fact = 1;
+public class Function {
 
-  for (int i = 1; i <= n; i++) {
-      fact *= i;
+  public static void main(String[] args) {
+    int result =  factorial(4);
+    System.out.println(result);
+
+    System.out.println("No is prime"+isPrime(7));
+    System.out.println("Prime numbers up to 20: " + listPrimeNo(20));
   }
 
-  return fact;
-  
-}
+   static int factorial(int n){
+        int fact = 1;
 
-void main(){
-  int result =  factorial(5);
-  system.out.println(result);
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+
+    }
+    static List<Integer> listPrimeNo(int n) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if(isPrime(i)){
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
 }
+    
+
+
+
